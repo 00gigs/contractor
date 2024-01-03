f759c50758ad9ce5dd06d4f37ea9ce247ccd4425
00gigs <124551005+00gigs@users.noreply.github.com>
2024-01-02 23:09:36 -0500

    titled the carousal component

--- src/Showcase.jsx
+++ src/Showcase.jsx
@@ -3,6 +3,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import k1 from './assets/kitchen1.jpeg'
 import k2 from './assets/kitchen2.jpeg'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 import bath1 from './assets/hotel-bathroom-interior-villa-preview.jpg'
 import bath2 from './assets/interior-deisgn-bathroom-modern-style-with-vanity_31965-122908.jpg.avif'
@@ -18,19 +19,23 @@ function Showcase() {
     return(
         <div className="accord">
         <Accordion defaultActiveKey="0">
-            <Accordion.Item eventKey="0" flush >
+            <Accordion.Item eventKey="0" flush>
                 <Accordion.Header>kitchens</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Body className="text-bg-dark">
                         <Carousel>
                             <Carousel.Item>
+                                <Link to="/Portfolio">
                                 <img className="Kitchen-img" src={k1} alt="" style={{ width:'100%', height: '300px',marginRight:'400px' }}/>
+                                </Link>
                                     <Carousel.Caption  className="custom-carousel-caption">
                                         <h6>Modern</h6>
                                         <p>Explore our gallery showcasing meticulous remodeling efforts. Expert craftsmanship, innovative design, and a commitment to perfection reflect the beauty of our kitchens, creating spaces that resonate with enduring charm.</p>
                                     </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item >
+                            <Link to="/Portfolio">
                                 <img className="Kitchen-img Kitchen-img-2" src={k2} alt="" style={{ width:'100%', height: '300px', marginRight:'400px'}}/>
+                                </Link>
                                     <Carousel.Caption  className="custom-carousel-caption">
                                         <h6>Modern Design 2</h6>
                                         <p>Explore our gallery showcasing meticulous remodeling efforts. Expert craftsmanship, innovative design, and a commitment to perfection reflect the beauty of our kitchens, creating spaces that resonate with enduring charm.</p>
@@ -42,17 +47,21 @@ function Showcase() {
             </Accordion.Item>
             <Accordion.Item eventKey="1" flush>
                 <Accordion.Header>Bathrooms</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Body className="text-bg-dark">
                         <Carousel>
                             <Carousel.Item>
+                            <Link to="/Portfolio">
                                 <img className="Kitchen-img" src={bath1} alt="" style={{ width:'100%', height: '300px',marginRight:'400px' }}/>
+                                </Link>
                                     <Carousel.Caption  className="custom-carousel-caption">
                                         <h6>minimalistic </h6>
                                         <p>Explore our gallery showcasing meticulous remodeling efforts. Expert craftsmanship, innovative design, and a commitment to perfection reflect the beauty of our kitchens, creating spaces that resonate with enduring charm.</p>
                                     </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item >
+                            <Link to="/Portfolio">
                                 <img className="Kitchen-img Kitchen-img-2" src={bath2} alt="" style={{ width:'100%', height: '300px', marginRight:'400px'}}/>
+                                </Link>
                                     <Carousel.Caption  className="custom-carousel-caption">
                                         <h6>minimalistic 2</h6>
                                         <p>Explore our gallery showcasing meticulous remodeling efforts. Expert craftsmanship, innovative design, and a commitment to perfection reflect the beauty of our kitchens, creating spaces that resonate with enduring charm.</p>
@@ -64,17 +73,21 @@ function Showcase() {
             </Accordion.Item>
             <Accordion.Item eventKey="2" flush>
                 <Accordion.Header>Decks</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Body className="text-bg-dark">
                         <Carousel>
                             <Carousel.Item>
+                            <Link to="/Portfolio">
                                 <img className="Kitchen-img" src={deck1} alt="" style={{ width:'100%', height: '300px',marginRight:'400px' }}/>
+                                </Link>
                                     <Carousel.Caption  className="custom-carousel-caption">
                                         <h3>WildCard</h3>
                                         <p>Explore our gallery showcasing meticulous remodeling efforts. Expert craftsmanship, innovative design, and a commitment to perfection reflect the beauty of our kitchens, creating spaces that resonate with enduring charm.</p>
                                     </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item >
+                            <Link to="/Portfolio">
                                 <img className="Kitchen-img Kitchen-img-2" src={deck2} alt="" style={{ width:'100%', height: '300px', marginRight:'400px'}}/>
+                                </Link>
                                     <Carousel.Caption  className="custom-carousel-caption">
                                         <h3>WildCard 2</h3>
                                         <p>Explore our gallery showcasing meticulous remodeling efforts. Expert craftsmanship, innovative design, and a commitment to perfection reflect the beauty of our kitchens, creating spaces that resonate with enduring charm.</p>
--- src/hero.jsx
+++ src/hero.jsx
@@ -1,10 +1,9 @@
 import Form from "react-bootstrap/Form";
-
+import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
 import Row from "react-bootstrap/Row";
-
 
 function Hero() {
   const Handleclick = () => {
@@ -14,13 +13,39 @@ function Hero() {
   return (
     <div className="hero-bg">
       <div className="left-side-hero">
-        <h1 class="display-6">
-          Are you ready to turn your dream home into a tangible masterpiece? Look no further! At Dream Homes Construction, we are committed to crafting exceptional living spaces that perfectly align with your unique lifestyle and preferences.
-        </h1>
+        <h1>Local reviews...</h1>
+        <Carousel>
+          <Carousel.Item>
+            <h1 class="display-6">
+              "New Horizon Builders delivered beyond expectations.
+              Professionalism, attention to detail, and stress-free
+              communication made our dream home a reality. Highly recommend
+              their expertise." Sarah Thompson, Happy Homeowner
+            </h1>
+          </Carousel.Item>
+          <Carousel.Item>
+            <h1 class="display-6">
+              "DreamCraft Homes exceeded expectations, delivering on time and
+              within budget. Their commitment to excellence and innovation made
+              our home-building experience truly impressive. Thank you!" Michael
+              and Emily Rodriguez, Satisfied Clients
+            </h1>
+          </Carousel.Item>
+          <Carousel.Item>
+            <h1 class="display-6">
+              Liberty Homes, a masterpiece in home construction. Meticulous
+              attention to detail, commitment to quality, and passion from the
+              team created our dream home. Highly recommend their top-notch
+              experience. - James and Lisa Anderson, Proud Homeowners
+            </h1>
+          </Carousel.Item>
+        </Carousel>
       </div>
       <div className="right-side-hero">
         <Form>
-          <Form.Text style={{ fontStyle: "oblique" }}>Fill out this form to discuss your ideas with a contractor today!</Form.Text>
+          <Form.Text style={{ fontStyle: "oblique" }}>
+            Fill out this form to discuss your ideas with a contractor today!
+          </Form.Text>
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Control type="email" placeholder="Enter email" />
