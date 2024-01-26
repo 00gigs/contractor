098273470ae662781a2fe4d0d481e0fb3437df90
00gigs <124551005+00gigs@users.noreply.github.com>
2024-01-25 20:15:43 -0500

    second commit

--- src/login.jsx
+++ src/login.jsx
@@ -1,26 +1,40 @@
-import {createClient} from './supabase/authconfig'
-import Cookies from 'universal-cookie';
+import {CreateClient} from './supabase/authconfig'
+import Cookies from 'js-cookie';
 import React from 'react'
+import {redirect} from 'react-router-dom'
 
 export const login = () => {
     
     async function signUpNewUser(formData) {
         "use server"
+        const origin = response.headers.get('origin');
         const email = formData.get('email')
         const password = formData.get('password')
+        const cookieStore = Cookies()
+        const supabase = CreateClient(cookieStore)
         const { error } = await supabase.auth.signUp({
           email ,
           password,
-          Cookies
+          emailRedirectTo:`${origin}/auth/callback`
         })
+        if(error){
+            return('error signing up user')
+        }
       }
       
-      async function signInWithEmail() {
+      async function signInWithEmail(formData) {
         "use server"
-        const { data, error } = await supabase.auth.signInWithPassword({
-          email: 'example@email.com',
-          password: 'example-password'
+        const email = formData.get('email')
+        const password = formData.get('password')
+       const cookieStore = Cookies()
+       const supabase =CreateClient(cookieStore)
+        const { error } = await supabase.auth.signInWithPassword({
+          email,
+          password
         })
+        if(error){
+            return('could not sign in user, please try again ',error)
+        }
         return('/')
       }
       
@@ -32,8 +46,10 @@ export const login = () => {
   return (
     <div>
         <form>
-            <input></input>
-            <input></input>
+            <input name='email' placeholder='email'/>
+            <input name='password' placeholder='password'/>
+            <button type='button' onClick={signUpNewUser}>Signup</button>
+            <button type='button' onClick={signInWithEmail}>Login</button>
         </form>
     </div>
   )
--- src/supabase/authconfig.js
+++ src/supabase/authconfig.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-export const  createClient=()=> {
+export const  CreateClient=()=> {
     createClient(
          supabaseUrl = process.env.SUPABASE_URL,
          supabaseKey = process.env.SUPABASE_ANON_KEY
